dd96fd0f14a9b7dad813386a13ebbac409b118d7
Orçun AŞIK <asikorcun@gmail.com>
2022-12-20 17:52:16 +0300

    2 Properties names fixed.

--- ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -9,8 +9,8 @@ namespace ProgrammersBlog.Shared.Entities.Abstract
     public abstract class EntityBase
     {
         public virtual int Id { get; set; }
-        public virtual DateTime CreatedTime { get; set; } = DateTime.Now;
-        public virtual DateTime ModifiedTime { get; set; } = DateTime.Now;
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
         public virtual bool IsDeleted { get; set; } = false;
         public virtual bool IsActive { get; set; } = true;
         public virtual string CreatedByName { get; set; } = "Admin";
