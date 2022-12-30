4678a08c86c47cdc95951ddbd255673cfe950fad
orcunasik <142536Orc>
2022-12-31 00:39:47 +0300

    Mvc Project was removed and added again, and required package and ConnectionString text for DbContext were declared in json file with Program.cs file.

--- ProgrammersBlog.Mvc/Program.cs
+++ ProgrammersBlog.Mvc/Program.cs
@@ -1,25 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ProgrammersBlogContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(ProgrammersBlogContext)).GetName().Name);
+    });
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
