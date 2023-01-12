5aaa7535d7245922115324ead7ea713fb70f1985
Orçun Aşık <asikorcun@gmail.com>
2023-01-12 16:30:33 +0300

    The created LoadMyServices has been reported to Program.cs

--- ProgrammersBlog.Mvc/Program.cs
+++ ProgrammersBlog.Mvc/Program.cs
@@ -1,16 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammersBlog.Services.Extensions;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ProgrammersBlogContext>(x =>
-{
-    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
-    {
-        option.MigrationsAssembly(Assembly.GetAssembly(typeof(ProgrammersBlogContext)).GetName().Name);
-    });
-});
+builder.Services.LoadMyServices();
+//builder.Services.AddDbContext<ProgrammersBlogContext>(x =>
+//{
+//    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+//    {
+//        option.MigrationsAssembly(Assembly.GetAssembly(typeof(ProgrammersBlogContext)).GetName().Name);
+//    });
+//});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
