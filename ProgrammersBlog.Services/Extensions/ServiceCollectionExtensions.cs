16ef21102b6e3af622061c4be3e6c472796f9ddd
Orçun Aşık <asikorcun@gmail.com>
2023-01-12 16:28:58 +0300

    A static class called named LoadMyServices has been created in IServiceCollection type.

---
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Data.Concrete;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Services.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ProgrammersBlogContext>(x =>
+            {
+                x.UseSqlServer(Configuration.ConnectionString);
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IArticleService, ArticleService>();
+            return services;
+        }
+    }
+}
