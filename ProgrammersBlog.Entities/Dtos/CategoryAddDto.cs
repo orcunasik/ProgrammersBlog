266d3a7a7eb1d065c125fb30bac08a2715caca83
Orçun Aşık <asikorcun@gmail.com>
2023-01-12 16:16:58 +0300

    Created Dto classes for Article and Category models

---
+++ ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        public string Name { get; set;}
+        public string Description { get; set;}
+        public bool IsActive { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ ProgrammersBlog.Entities/Dtos/CategoryUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class CategoryUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Note { get; set; }
+    }
+}
