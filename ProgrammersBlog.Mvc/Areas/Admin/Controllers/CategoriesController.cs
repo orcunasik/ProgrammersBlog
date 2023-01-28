61603bbc3ac353f5bd0d0ee42437b3e0c2243a13
Orçun Aşık <asikorcun@gmail.com>
2023-01-29 00:28:46 +0300

    Added CategoriesController

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+            return View(result.Data);
+        }
+    }
+}
