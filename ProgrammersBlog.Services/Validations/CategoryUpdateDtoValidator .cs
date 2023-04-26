ca7a870c825a3400d21b50ded76637ef9762ed53
Orçun Aşık <asikorcun@gmail.com>
2023-04-26 19:26:02 +0300

    Refactored

--- ProgrammersBlog.Services/Validations/CategoryUpdateDtoValidator .cs
+++ ProgrammersBlog.Services/Validations/CategoryUpdateDtoValidator .cs
@@ -26,10 +26,10 @@ namespace ProgrammersBlog.Services.Validations
             RuleFor(c => c.Note).MinimumLength(3).WithName("Kategori Özel Not").WithMessage("{PropertyName} Alanı 3 Karakterden Az Olmamalıdır!");
 
             RuleFor(c => c.IsActive).NotNull().WithName("Aktif Mi?").WithMessage("{PropertyName} Alanı Gereklidir!");
-            RuleFor(c => c.IsActive).NotEmpty().WithName("Aktif Mi?").WithMessage("{PropertyName} Boş Geçilmemelidir!");
+            //RuleFor(c => c.IsActive).NotEmpty().WithName("Aktif Mi?").WithMessage("{PropertyName} Boş Geçilmemelidir!");
 
             RuleFor(c => c.IsDeleted).NotNull().WithName("Silindi Mi?").WithMessage("{PropertyName} Alanı Gereklidir!");
-            RuleFor(c => c.IsDeleted).NotEmpty().WithName("Silindi Mi?").WithMessage("{PropertyName} Boş Geçilmemelidir!");
+            //RuleFor(c => c.IsDeleted).NotEmpty().WithName("Silindi Mi?").WithMessage("{PropertyName} Boş Geçilmemelidir!");
         }
     }
 }
