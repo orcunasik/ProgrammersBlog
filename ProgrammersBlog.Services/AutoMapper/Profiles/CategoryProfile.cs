2c97f43ae94bcf50f5c2436ef5323c811c4aa926
Orçun Aşık <asikorcun@gmail.com>
2023-04-26 19:28:55 +0300

    Category was mapped to CategoryUpdateDto.

--- ProgrammersBlog.Services/AutoMapper/Profiles/CategoryProfile.cs
+++ ProgrammersBlog.Services/AutoMapper/Profiles/CategoryProfile.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using ProgrammersBlog.Entities.Concrete;
 using ProgrammersBlog.Entities.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Services.AutoMapper.Profiles
 {
@@ -15,6 +10,7 @@ namespace ProgrammersBlog.Services.AutoMapper.Profiles
         {
             CreateMap<CategoryAddDto, Category>().ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now));
             CreateMap<CategoryUpdateDto, Category>().ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<Category, CategoryUpdateDto>();
         }
     }
 }
