922e5cd8bc522eb5709fe9e8eb5ab9a3fe9ac2f3
Orçun Aşık <asikorcun@gmail.com>
2023-06-06 14:32:26 +0300

    Removed unsed "usings" and comment lines.

--- ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProgrammersBlog.Data.Concrete.EntityFramework.Mappings;
 using ProgrammersBlog.Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
 {
@@ -20,10 +15,7 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer(@"Server=DESKTOP-QEHR73R\SQLEXPRESS;Database=ProgrammersBlog;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-        //}
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ArticleMap());
