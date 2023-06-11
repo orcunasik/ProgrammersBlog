293b70ab4c774ac2ce881ebe3d40b7b02e92febb
Orçun Aşık <asikorcun@gmail.com>
2023-06-11 23:36:38 +0300

    Created UserController, User Index  and UserListDto classes.

---
+++ ProgrammersBlog.Entities/Dtos/UserListDto.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserListDto : DtoGetBase
+    {
+        public IList<User> Users { get; set; }
+    }
+}
