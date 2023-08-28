6570457aad8fb637161621b23ab7ab04b1c4fc15
Orçun Aşık <asikorcun@gmail.com>
2023-08-28 22:53:46 +0300

    Created has been UserDto

---
+++ ProgrammersBlog.Entities/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserDto : DtoGetBase
+    {
+        public User User { get; set; }
+    }
+}
