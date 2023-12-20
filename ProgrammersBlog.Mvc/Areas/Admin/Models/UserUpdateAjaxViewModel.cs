726db162b25645fc75e6b2b4b1146082e95c99ec
Orçun Aşık <asikorcun@gmail.com>
2023-12-20 23:50:37 +0300

    UserUpdateAjaxViewModel added for userIndex update method

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/UserUpdateAjaxViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammersBlog.Entities.Dtos;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models;
+
+public class UserUpdateAjaxViewModel
+{
+    public UserUpdateDto UserUpdateDto { get; set; }
+    public string UserUpdatePartial { get; set; }
+    public UserDto UserDto { get; set; }
+}
