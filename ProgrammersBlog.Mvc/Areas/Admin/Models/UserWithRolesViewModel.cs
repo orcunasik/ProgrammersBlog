27f7bbe37903c79e2f6fbbbc30898775e1566fcf
Orçun Aşık <asikorcun@gmail.com>
2023-12-20 23:47:36 +0300

    AdminMenuViewComponent and UserWithRolesViewModel classes were created.

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models;
+
+public class UserWithRolesViewModel
+{
+    public User User { get; set; }
+    public IList<string> Roles { get; set; }
+}
