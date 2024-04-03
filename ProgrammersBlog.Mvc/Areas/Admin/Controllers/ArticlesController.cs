91540e023f22aee990cedc601707cfea65772ac8
Orçun Aşık <asikorcun@gmail.com>
2024-04-03 20:48:09 +0300

    Added Article controller

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/ArticlesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class ArticlesController : Controller
+{
+    private readonly IArticleService _articleService;
+
+    public ArticlesController(IArticleService articleService)
+    {
+        _articleService = articleService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var result = await _articleService.GetAllByNonDeletedAsync();
+        if (result.ResultStatus == ResultStatus.Success)
+            return View(result.Data);
+        return NotFound();
+    }
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+}
