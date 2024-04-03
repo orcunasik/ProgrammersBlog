85cb53a78bda5e203e80e62fd1d381fcefb56d0c
Orçun Aşık <asikorcun@gmail.com>
2024-04-03 20:52:42 +0300

    Added articleAdd.js file for Article Add page

---
+++ ProgrammersBlog.Mvc/wwwroot/AdminAreas/js/articlesAdd.js
@@ -0,0 +1,33 @@
+﻿jQuery(function ($) {
+
+    //Trumbowyg Starting
+    $('#text-editor').trumbowyg({
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['foreColor', 'backColor'],
+            ['emoji'],
+            ['fontfamily'],
+            ['fontsize']
+        ]
+    });
+    //Trumbowyg Ending
+
+    //Select2 Starting
+    $('#categoryList').select2({
+        theme: 'bootstrap4',
+        placeholder: "Lütfen bir kategori seçiniz!",
+        allowClear: true
+    });
+    //Trumbowyg Edning
+});
